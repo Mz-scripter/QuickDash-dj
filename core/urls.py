3975b19780ed44ab17b76b54e0be0681e5a5acab
Adeko Muheez <mzscripterx5@gmail.com>
2024-11-20 14:26:47 +0100

    Added app_name varibale in the apps urls.

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'core'
+
 urlpatterns = [
     path('', views.homePage, name='home'),
     path('contact/', views.contactPage, name='contact')
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'users'
+
 urlpatterns = [
     path('login/', views.loginPage, name='login'),
     path('register/', views.registerPage, name='register'),
