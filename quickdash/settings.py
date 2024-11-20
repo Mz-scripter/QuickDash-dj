928d944b4a66424088cab427c64405ac776f8c30
Adeko Muheez <mzscripterx5@gmail.com>
2024-11-20 13:55:48 +0100

    Set up frontend templates for the users and order apps

--- quickdash/settings.py
+++ quickdash/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'core',
+    'users',
+    'orders',
 ]
 
 MIDDLEWARE = [
@@ -115,8 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
