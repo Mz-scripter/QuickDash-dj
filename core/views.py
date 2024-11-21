0d4d973d5af59532dfb9d7ffcd4e711cdad29ec5
Adeko Muheez <mzscripterx5@gmail.com>
2024-11-21 15:03:58 +0100

    Created Item model to display items

--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from orders.models import Item
 
 def homePage(request):
-    return render(request, 'core/index.html')
+    items = Item.objects.all()
+    context = {'items': items}
+    return render(request, 'core/index.html', context)
 
 def contactPage(request):
     return render(request, 'core/contact.html')
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    restaurant = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
+    image = models.ImageField(upload_to='menu_images/')
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
