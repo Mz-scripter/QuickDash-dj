5847fbe71d6a5c0fca5acfc791c989f47df29fa9
Adeko Muheez <mzscripterx5@gmail.com>
2024-11-22 18:38:57 +0100

    Created User registration and login

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.CharField(max_length=150, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
