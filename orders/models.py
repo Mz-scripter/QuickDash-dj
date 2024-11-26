8cc58e7113cbee96fce6608fdf415e206c7acc4c
Adeko Muheez <mzscripterx5@gmail.com>
2024-11-26 12:34:09 +0100

    Users can now add item to their cart.

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
@@ -11,3 +12,16 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class CartItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.item.name}"
+    
+    def get_total_price(self):
+        return self.quantity * self.item.price
--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Item, CartItem
 
+
+@login_required(login_url='login')
+def add_to_cart(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    cart_item, created = CartItem.objects.get_or_create(user=request.user, item=item)
+    if not created:
+        cart_item.quantity += 1
+    cart_item.save()
+    return redirect('home')
+
+@login_required(login_url='login')
 def cartPage(request):
-    return render(request, 'orders/cart.html')
+    cart_items = CartItem.objects.filter(user=request.user)
+    total_price = sum(item.get_total_price() for item in cart_items)
+    context = {'cart_items': cart_items, 'total_price': total_price}
+    return render(request, 'orders/cart.html', context)  
