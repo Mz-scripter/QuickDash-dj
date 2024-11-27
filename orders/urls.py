8877c1dced43d633b7baa1a92ba546af9273a5d4
Adeko Muheez <mzscripterx5@gmail.com>
2024-11-27 08:17:58 +0100

    Users can remove, increase and decrease cart items.

--- orders/urls.py
+++ orders/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('add-to-cart/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.cartPage, name='cart'),
+
+    path('cart/remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('cart/increase/<int:item_id>/', views.increase_quantity, name='increase_quantity'),
+    path('cart/decrease/<int:item_id>/', views.decrease_quantity, name='decrease_quantity'),
 ]
