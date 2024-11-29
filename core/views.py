a8a37e9b6e16ae5aa3c0ad93c279ef6248fe8b89
Adeko Muheez <mzscripterx5@gmail.com>
2024-11-29 09:05:01 +0100

    Added search functionality.

--- core/views.py
+++ core/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from orders.models import Item, CartItem, WishListItem
-from django.db.models import Sum
+from django.db.models import Sum, Q
 
 def homePage(request):
-    items = Item.objects.all()
-    user_wishlist = set()
+    query = request.GET.get('q', '')
+    items = Item.objects.filter(
+        Q(name__icontains=query) |
+        Q(description__icontains=query)
+    )
+    user_wishlist = set()        
+    total_items = 0
     if request.user.is_authenticated:
         user_wishlist = set(
             WishListItem.objects.filter(user=request.user).values_list('item_id', flat=True)
         )
-    total_items = 0
-    if request.user.is_authenticated:
         total_items = CartItem.objects.filter(user=request.user).aggregate(total=Sum('quantity'))['total'] or 0
     context = {'items': items, 'total_items': total_items, 'user_wishlist': user_wishlist or None}
     return render(request, 'core/index.html', context)
