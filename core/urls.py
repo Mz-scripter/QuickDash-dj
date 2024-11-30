9b88e51a5864105c5fb446fc27b0665feaffaab4
Adeko Muheez <mzscripterx5@gmail.com>
2024-11-30 12:06:56 +0100

    Added autocomplete suggestions to the search bar.

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.homePage, name='home'),
+
+    path('autocomplete/', views.autocomplete, name='autocomplete'),
+
     path('contact/', views.contactPage, name='contact')
 ]
